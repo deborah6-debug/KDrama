0a2f53aeb1ee871541870f7150c1abca7a48bd81
deborah6-debug <chisomdeborah14@gmail.com>
2024-01-22 13:38:31 +0100

    First Commit

---
+++ src/MarvelData.js
@@ -0,0 +1,114 @@
+export const characters = [
+  // Iron Man
+  {
+    name: "Tony Stark",
+    alias: "Healer",
+    img1: "https://encrypted-tbn1.gstatic.com/images?q=tbn:ANd9GcSJBkjJSaZM8tijzaYRCoyOx7j8ykC0dDJa9kp6n3Q6gx6F3yZl",
+    // homeworld: "Earth",
+    abilities: [
+      "A decades-old incident involving a group of five friends who ran an illegal pro-democracy broadcasting station during the Fifth Republic in South Korea",
+      "brings together three different people—an illegal  with the codename-Healer  who possesses top-notch fighting skills",
+      "a reporter from a second-rate tabloid news website , and a famous journalist at a major broadcast station While trying to uncover the truth from that 1992 incident",
+      "and a series of present day murders, they grow into honest reporters who try to blur the lines of conflict between truth and reality, even if that means fighting media honchos.",
+    ],
+  },
+  {
+    name: "Kwak Jung-hwan",
+    alias: "The K2",
+    abilities: [
+    "Kim Je Ha is a former soldier for hire. He is also called K2.", 
+    "He is hired as a bodyguard by Choi Yoo Jin. Choi Yoo Jin is the wife of Jang Se Joon, who is running for president.", 
+    "Meanwhile, Go Anna is the reclusive illegitimate daughter of Jang Se Joon whom Je Ha has to guard, which he later fell in love with.",
+  ],
+    img1: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcR5I_KyQljV9l7iX0hJ3510jNj11hX7roGVejgSj5vVzc-tYAvJ",
+  },
+  {
+    name: "Jin Hyuk",
+    alias: "City Hunter",
+    abilities: ["The story revolves around Lee Yoon-sung, played by Lee Min-ho, who is raised by his father to become a skilled martial artist and expert marksman. However, his life takes a dramatic turn when his father, a dedicated government official, is betrayed and killed by his own colleagues. To avenge his father's death and uncover the truth behind the betrayal, Yoon-sung adopts the persona of City Hunter"],
+    img1: "https://encrypted-tbn1.gstatic.com/images?q=tbn:ANd9GcR4NYSKzmxBzBlVOSv69JAlxV_YGI9oLXXNhuOQ8948Ts9utci6",
+  },
+
+  // Spider-Man
+  {
+    name: "Hong Jong-chan",
+    alias: "Her Private Life",
+    abilities: ["The drama explores themes of identity, love, and the challenges of balancing personal interests with professional life. It received positive reviews for its lighthearted and humorous approach to romance and the engaging performances of the cast. For the most accurate and detailed summary, I recommend checking the latest sources or watching the series directly."],
+    img1: "https://encrypted-tbn3.gstatic.com/images?q=tbn:ANd9GcSxhbSL1Tt8Fzo0wn5YmWC6fsKPK4yZenehPZUElKMyPwjJFqRc",
+  },
+
+  {
+    name: "Kim Hee-won",
+    alias: "Vincenzo",
+    abilities: ["Vincenzo teams up with Hong Cha-young, played by Jeon Yeo-bin, who is a determined and resourceful lawyer. Together, they work to bring justice to the victims of Babel Group's wrongdoing. The drama combines elements of dark comedy, crime, and legal thriller, with Vincenzo using his Mafia background to navigate the complex world of corruption and corporate crime."],
+    img1: "https://encrypted-tbn1.gstatic.com/images?q=tbn:ANd9GcTaD6zet1fGlmjQQrSI0QXW-IzJ4H9LkrbpD7C-gfXdmiJ6IeUx",
+  },
+  {
+    name: "Kown Young-il",
+    alias: "Doom At Your Service",
+    abilities: ["Doom at Your Service tells the story of Tak Dong-kyung, played by Park Bo-young, a woman who has been dealt a difficult hand in life. She receives a terminal diagnosis and, feeling helpless and desperate, she expresses her frustration by wishing for the world to end. To her surprise, her wish is answered by Myul Mang, played by Seo In-guk, an otherworldly being known as a destroyer"],
+    img1: "https://encrypted-tbn2.gstatic.com/images?q=tbn:ANd9GcTMb-7QUGzhmcZBHTCD4GNJaDs0vLdzNNA99ro8y5Zd88TSb9d5",
+  },
+  {
+    name: "Kim Byung Soo",
+    alias: "Bride Of Habeak",
+    abilities: ["The drama weaves in elements of romance, fantasy, and political intrigue as various divine and mortal characters become entangled in the fate of the water god and his bride. Themes of love, sacrifice, and the clash between the divine and mortal worlds are central to the narrative."],
+    img1: "https://encrypted-tbn3.gstatic.com/images?q=tbn:ANd9GcTLeTQlz3h9NSwfRIXjZAT0x1BsVgE2Eg59p4HyKTU1JP0MJ-8m",
+  },
+  {
+    name: "Kim Jin-min",
+    alias: "Lawless Lawyer",
+    abilities: ["As Sang-pil and Ha Jae-yi delves into his cases, he uncovers the truth behind his mother's death and seeks revenge against those responsible. The drama combines legal elements with action and suspense, creating a thrilling narrative that explores themes of justice, redemption, and the battle against systemic corruption."],
+    img1: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcR4bTnGakjg2rAmztUiWEaKzl__PZQd-Gp1PEQQIeqsShxnkz6o",
+  },
+  {
+    name: "Nam Ki-Hoon",
+    alias: "Kiss Sixth Sense",
+    abilities: ["The series depicts the story of Hong Ye-sool, a woman who has the supernatural ability to see the future when she is kissed, accidentally kisses her boss, Cha Min-hoo, and sees their future together and her boss whose has the abilty to hear sounds from a wide distance. Her life gets more entangled when Ye-sool’s ex-boyfriend Lee Pil-yo comes back into her life. The show stars Yoon Kye-sang as Cha Min-hoo, Seo Ji-hye as Hong Ye-sool, and Kim Ji-seok as Lee Pil-yo"],
+    img1: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQbzJWzDCH2SH7v950b1DxsGpr68bMjpCCkE6CXroDCW_fWjIzY",
+  },
+  {
+    name: "Nam Sung-woo",
+    alias: "Love In Contract",
+    abilities: ["The series depicts the story of Choi Sang-eun, a contract marriage master who transforms herself into the perfect partner for her clients, but unexpectedly falls for two men who use her services. The show stars Park Min-young as Choi Sang-eun, Go Kyung-pyo as Jung Ji-ho, and Kim Jae-young as Kang Hae-jin"],
+    img1: "https://upload.wikimedia.org/wikipedia/en/2/20/Love_in_Contract.jpeg",
+  },
+  {
+    name: "Kim Ga-ram",
+    alias: "Nevertheless",
+    abilities: ["Nevertheless follows the complicated romantic relationship between Yoo Na-bi, played by Han So-hee, and Park Jae-eon, portrayed by Song Kang. Na-bi is a university student majoring in sculpture who, after experiencing a difficult breakup, becomes skeptical about love and relationships. On the other hand, Jae-eon is a charming and free-spirited art major known for his good looks and nonchalant attitude towards dating."],
+    img1: "https://encrypted-tbn1.gstatic.com/images?q=tbn:ANd9GcSQZKfkQElQJJBLLKmngUWEmXdSJQX0-h9Uw4xC72PtAGYAmAyF",
+  },
+  {
+    name: "Park Joon-hwa",
+    alias: "What Is Wrong With Sectary Kim",
+    abilities: ["The drama explores their evolving relationship as Young-joon attempts to understand his feelings and Mi-so grapples with her decision to leave.The series combines elements of romance and comedy, with a focus on the personal growth of the characters. As the two protagonists navigate their emotions, the drama also delves into the mysteries of Mi-so past and the reasons behind her dedication to her work."],
+    img1: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcStILmCZuuWw4l4PYJyZKnTUk22Zfcrj0WNu6nxYC9n5Db1PTRi",
+  },
+  {
+    name: "Park Shin-woo",
+    alias: "It's Okay To Not Be",
+    abilities: ["The drama explores themes of mental health, trauma, and the healing power of relationships. It delves into the characters' pasts, allowing for a deeper understanding of their struggles and the impact of their shared experiences. The romance that develops between Kang-tae and Moon-young is central to the narrative, providing a balance of emotional depth and heartwarming moments."],
+    img1: "https://upload.wikimedia.org/wikipedia/en/5/58/It%27s_Okay_to_Not_Be_Okay_Poster.jpg",
+  },
+  {
+    name: "Nam Sung woo",
+    alias: "My Lovely Liar",
+    abilities: ["The series depicts the story of Mok Sol-hee, a woman who can’t trust people because of her ability to hear lies, and uncovers the truth while getting entangled with a murder suspect whose innocence no one believes in"],
+    img1: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcT0Vf-teoJ3wJlbMixvP3VYoByB7SmBrE5bjjSjohFCfvKzs9a_",
+  },
+  {
+    name: "Kim Jang-han and Kowo Da-som",
+    alias: "My Demon",
+    abilities: ["The series depicts the story of Do Do-hee, the CEO of Mirae F&B, who enters into a contract marriage with Jeong Gu-won, a demon who temporarily loses his powers. The show stars Kim Yoo-jung as Do Do-hee and Song Kang as Jeong Gu-won."],
+    img1: "https://i.mydramalist.com/0w0mZ6_4f.jpg",
+  },
+  {
+    name: "Joo Dong-min",
+    alias: "The Penthouse: War in Life",
+    abilities: ["The series depicts the story of wealthy families living in Hera Palace and their children at Cheong-ah Arts School. Shim Su-ryeon (Lee Ji-ah) is an elegant, wealthy woman who has a tragic past. Her husband is Joo Dan-tae (Uhm Ki-joon), a successful businessman. She later learns that he is hiding a secret from her."],
+    img1: "https://th.bing.com/th/id/R.9acbe187983fa82a293de6c83ea3121c?rik=byib1DCCNtXaNA&pid=ImgRaw&r=0",
+  },
+];
+
+console.log(characters);
